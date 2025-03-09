99d1d3dd6cf19b58e3d28d43f4ebcedac2ab376e
toploardgg <156077554+toploardgg@users.noreply.github.com>
2025-03-09 15:20:45 +0200

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -31,3 +31,43 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+const translations = {
+    en: {
+        greeting: "Hi, how are you?",
+        support: "Support us:",
+        donate: "Donate with PayPal",
+        contact: "Contacts:",
+        contacts: "Gmail: Toploardgg@gmail.com & Telegram: Loardstk",
+        name: "Toploardgg (Stas)",
+        language: "English"
+    },
+    ru: {
+        greeting: "Привет, как дела?",
+        support: "Поддержать нас:",
+        donate: "Пожертвовать через PayPal",
+        contact: "Контакты:",
+        contacts: "Gmail: Toploardgg@gmail.com & Telegram: Loardstk",
+        name: "Toploardgg (Стас)",
+        language: "Русский"
+    },
+    uk: {
+        greeting: "Привіт, як справи?",
+        support: "Підтримайте нас:",
+        donate: "Пожертвувати через PayPal",
+        contact: "Контакти:",
+        contacts: "Gmail: Toploardgg@gmail.com & Telegram: Loardstk",
+        name: "Toploardgg (Стас)",
+        language: "Українська"
+    }
+};
+
+function changeLanguage(lang) {
+    document.getElementById("greeting").textContent = translations[lang].greeting;
+    document.getElementById("support").textContent = translations[lang].support;
+    document.getElementById("donate").textContent = translations[lang].donate;
+    document.getElementById("name").textContent = translations[lang].name;
+    document.getElementById("contacts").textContent = translations[lang].contacts;
+    document.getElementById("contact").textContent = translations[lang].contact;
+    document.getElementById("language-button").textContent = translations[lang].language;
+}
